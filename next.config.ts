95bd6087169c931531f26019fb8d6666c0c87a6c
dkoti222 <dkoti22222@gmail.com>
2026-05-09 18:18:24 +0530

    updated portfolio

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-    eslint: {
-      ignoreDuringBuilds: true, // ✅ This disables linting errors during build
-    },
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
